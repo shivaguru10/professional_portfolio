df5722f4b74a7443dc5c88b46fea3e5b03c4bd1c
Shivaguru10 <gsshivaguru@gmail.com>
2026-02-26 12:25:07 +0530

    fix: change email link to open gmail directly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -54,7 +54,9 @@ export default function Footer() {
           </a>
 
           <a
-            href="mailto:gsshivaguru@gmail.com?subject=Portfolio%20Inquiry%20-%20Job%20Opportunity&body=Hi%20Shivaguru,%0D%0A%0D%0AI%20came%20across%20your%20portfolio%20and%20wanted%20to%20reach%20out...%0D%0A%0D%0A"
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=gsshivaguru@gmail.com&su=Portfolio%20Inquiry%20-%20Job%20Opportunity&body=Hi%20Shivaguru,%0A%0AI%20came%20across%20your%20portfolio%20and%20wanted%20to%20reach%20out..."
+            target="_blank"
+            rel="noopener noreferrer"
             className="text-[#777777] hover:text-[#111111]"
             title="Email"
           >
